c6f65077a7bd647ba8107e04fc64b5c953cdb492
jdrbrn <jdrbrn@gmail.com>
2017-12-06 10:36:26 -0500

    Moved most functions of index.html to report/index.html; Set report/index.html to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'dncs/print'
+  get 'report/index'
   get 'report/print'
   get 'report/history'
   resources :report
+  root 'report#index'
 
   resources :dncs
   resources :terrouts
