98f8eb6b3ff7ec6409f71b886420dfef83bf5173
jdrbrn <jdrbrn@gmail.com>
2018-01-17 17:14:52 -0500

    Updated UpdateLocal.sh to updated any related assests and restart the passenger app

--- UpdateLocal.sh
+++ UpdateLocal.sh
@@ -2,3 +2,6 @@ git checkout -b local
 git add .
 git commit -m "Saving any local changes"
 git merge origin/release -m "Updating to latest release code"
+bundle install --deployment --without development test
+bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+passenger-config restart-app $(pwd)
