3655dc34bcdd5e79ab32dd92979070795bdc7290
jdrbrn <jdrbrn@gmail.com>
2018-01-20 19:59:15 -0500

    Working on UpdateLocal.sh

--- UpdateLocal.sh
+++ UpdateLocal.sh
@@ -1,6 +1,9 @@
 git checkout -b local
 git add .
 git commit -m "Saving any local changes"
+echo "Downloading latest changes"
+git fetch
+echo "Updating to latest release"
 git merge origin/release -m "Updating to latest release code"
 bundle install --deployment --without development test
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
