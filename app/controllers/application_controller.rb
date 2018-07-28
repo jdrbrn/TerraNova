62dfbb2f7ac97ac2e229cf35248d0e02d84f5751
jdrbrn <jdrbrn@gmail.com>
2018-07-27 21:51:34 -0400

    Added line for basic http auth in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # To enable username/password authentication uncomment the bellow and set the username and password
+  # http_basic_authenticate_with name: "", password: "password"
 end
