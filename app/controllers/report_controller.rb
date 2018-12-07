2bf2b8b12c20b84939b78c4f89686877148bb5f2
jdrbrn <jdrbrn@gmail.com>
2018-12-07 17:21:59 -0500

    Removed debugging line from report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,7 +3,6 @@ class ReportController < ApplicationController
   def index
     # If in development reload TerraNovaConfig when loading report/index
     if Rails.env.development?
-      puts Dir.pwd
       puts "Reloading TerraNovaConfig.rb"
       load "#{Dir.pwd}/config/TerraNovaConfig.rb"
       Object.const_set("TerraNovaConfig", loadConfig)
