2b9db22367d5039e97732a7c3392734e9ad68f7e
jdrbrn <jdrbrn@gmail.com>
2020-05-05 13:09:42 -0400

    report/print terrout table is generated from config

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -29,40 +29,110 @@ module ReportHelper
     def fullname(territory)
         terrid = getID(territory)
 
-        output = "<td class=\""+deadlineCSS(territory)+"\">"
+        output = ""
         output += @terrs.find(terrid).name + "<br>" + @terrs.find(terrid).region
-        output += "</td>"
         output.html_safe
     end
 
     def datecomp(territory)
         terrid = getID(territory)
 
-        output = "<td>"
+        output = ""
         output += @terrs.find(terrid).datecomp.strftime("%m/%d/%y")
-        output += "</td>"
         output.html_safe
     end
 
     # Terrout Functions
     def publisher(terrout)
-        output = "<td>"
+        output = ""
         output += terrout.publisher
-        output += "</td>"
         output.html_safe
     end
 
     def dateout(terrout)
-        output = "<td>"
+        output = ""
         output += terrout.dateout.strftime("%m/%d/%y")
-        output += "</td>"
         output.html_safe
     end
     
     def datedue(terrout)
-        output = "<td>"
+        output = ""
         output += terrout.datedue.strftime("%m/%d/%y")
-        output += "</td>"
+        output.html_safe
+    end
+
+    # Table Gen Functions
+    def testing
+        return "TESTING"
+    end
+
+    def runFunctionTH(functionString)
+        # TH doesn't pass any paramaters so check if function
+        # wants paramaters, if so don't call
+        if respond_to?(functionString.to_sym)
+            if method(functionString).parameters==[]
+                method(functionString).call
+            end
+        end
+    end
+
+    def runFunctionTD(functionString, territory)
+        if respond_to?(functionString)
+            if method(functionString).parameters!=[]
+                method(functionString).call(territory)
+            else
+                method(functionString).call
+            end
+        end
+    end
+
+    def generateTH(config)
+        output="<th"
+
+        if config["class"]
+            output+=" class=\""
+            if config["class"]["html"]
+                output+=config["class"]["html"]
+            elsif config["class"]["function"]
+                output+=runFunctionTH(config["class"]["function"])
+            end
+            output+="\""
+        end
+        output+=">"
+
+        if config["html"]
+            output+=config["html"]
+        elsif config["function"]
+            output+=runFunctionTH(config["function"])
+        end
+
+        output+="</th>"
+
+        output.html_safe
+    end
+    
+    def generateTD(config, territory)
+        output="<td"
+
+        if config["class"]
+            output+=" class=\""
+            if config["class"]["html"]
+                output+=config["class"]["html"]
+            elsif config["class"]["function"]
+                output+=runFunctionTD(config["class"]["function"], territory)
+            end
+            output+="\""
+        end
+        output+=">"
+
+        if config["html"]
+            output+=config["html"]
+        elsif config["function"]
+            output+=runFunctionTD(config["function"], territory)
+        end
+
+        output+="</td>"
+
         output.html_safe
     end
 end
