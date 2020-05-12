8c9ec5168a650b3f16de7cdf3044f812acb1cd73
jdrbrn <jdrbrn@users.noreply.github.com>
2020-05-12 15:07:47 -0400

    Updated TerraNovaConfig.rb
    Removed DevTerraNovaConfig.json
    Reworked how configs are loaded + checked
    Added support for user defined keys

--- config/TerraNovaConfig.rb
+++ config/TerraNovaConfig.rb
@@ -2,55 +2,53 @@ require 'json'
 
 def loadConfig
   # Loads default config
-  # Sets what keys should be found for prefs and default values
-  config=JSON.parse(File.read("config/default.json"))
+  # Sets what keys need to exist and default values
+  default=JSON.parse(File.read("config/default.json"))
   
   #File name to use for the configFile
   configFileName="TerraNovaConfig.json"
-  # If in development mode load a dev config file and save a default config with the latest defaults
-  if Rails.env.development?
-    configFile=File.new(configFileName,"w")
-    configFile.puts(JSON.pretty_generate(config))
-    configFile.close
-    configFileName="Dev#{configFileName}"
-  end
+
   #Checks to see if config file exists and creates if not
   if File.file?(configFileName)==false
     File.new(configFileName,"a+").close
   end
+
   #Loads the JSON File
   #Error handling for if there is an issue with the JSON data(ex: doesn't exist)
   #Which resets to a blank/default config
   begin
-    loadedConfig=JSON.parse(File.read(configFileName))
+    config=JSON.parse(File.read(configFileName))
   rescue JSON::ParserError
-    loadedConfig={}
+    config={}
   end
-  #Checks for the version of TerraNova used to save the config file
-  #This is then removed so that the current version can be written out
-  #This block could also contain migration logic based on version if needed(Ex: If a key is renamed)
-  if loadedConfig["TerraNovaVersion"]
-    loadedConfig.delete("TerraNovaVersion")
+
+  # Checks for the version of TerraNova used to last save the config file
+  # Version number is then updated to be the current version number
+  # This block could also contain migration logic based on version if needed(Ex: If a key is renamed)
+  if config["TerraNovaVersion"]
+    config["TerraNovaVersion"]=default["TerraNovaVersion"]
   end
-  #For each config value read from the file it loads the value into our config var
-  #This way if there is supposed to be a config value but isn't, the default is used
-  #Removes arbitary data stored in config.json that isn't called for
-  loadedConfig.each do |key|
-    # Check to see if the key should exist and loadedConfig contains the type of value it should
-    if config[key[0]] && config[key[0]].class == loadedConfig[key[0]].class
-      # If hash or array merge the two so new keys get added to the config file
-      if config[key[0]].class == Hash
-        loadedConfig[key[0]].each do |hashKey|
+
+  # Goes through each default key to verify that all needed keys exist and are valid types
+  default.each do |key|
+    # Check to see if the key exists and is a valid type
+    if config[key[0]] && config[key[0]].class == default[key[0]].class
+      # If hash or array make sure all required values are included
+      if default[key[0]].class == Hash
+        default[key[0]].each do |hashKey|
           # Check to see if the hashKey should exist and loadedConfig contains the type of value it should
-          if config[key[0]][hashKey[0]] && config[key[0]][hashKey[0]].class == loadedConfig[key[0]][hashKey[0]].class
-            config[key[0]][hashKey[0]]=loadedConfig[key[0]][hashKey[0]]
+          # If not, the value is reset
+          if config[key[0]][hashKey[0]] && config[key[0]][hashKey[0]].class != default[key[0]][hashKey[0]].class
+            config[key[0]][hashKey[0]]=default[key[0]][hashKey[0]]
           end
         end
-      else
-        config[key[0]]=loadedConfig[key[0]]
       end
+    # If key doesn't exist copy it into user config
+    else
+      config[key[0]]=default[key[0]]
     end
   end
+
 
   #Deletes the existing config and then saves the loaded config
   #This makes sure to easily add any keys that were missing or changed
