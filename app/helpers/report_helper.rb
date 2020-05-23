5e3c6cf0d854a9e612406d31a819df39838e7123
jdrbrn <jdrbrn@users.noreply.github.com>
2020-05-22 23:38:24 -0400

    Updated territory table function names

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,6 +1,6 @@
 module ReportHelper
     # Territory Functions
-    def getID(territory)
+    def territoryGetID(territory)
         if territory.class != Terr
             territory.terrid
         else
@@ -8,7 +8,7 @@ module ReportHelper
         end
     end
 
-    def deadlineCSS(territory)
+    def territoryDeadlineCSS(territory)
         output = nil
 
         if territory.class == Terrout
@@ -25,70 +25,72 @@ module ReportHelper
         output
     end
 
-    def fullname(territory)
+    def territoryFullname(territory)
         output = ""
-        output += name(territory) + "<br>" + region(territory)
+        output += territoryName(territory) + "<br>" + territoryRegion(territory)
     end
 
-    def datecomp(territory)
-        terrid = getID(territory)
+    def territoryDatecomp(territory)
+        terrid = territoryGetID(territory)
 
         output = ""
         output += @terrs.find(terrid).datecomp.strftime("%m/%d/%y")
     end
 
-    def name(territory)
-        terrid = getID(territory)
+    def territoryName(territory)
+        terrid = territoryGetID(territory)
 
         output = ""
         output += @terrs.find(terrid).name
     end
     
-    def region(territory)
-        terrid = getID(territory)
+    def territoryRegion(territory)
+        terrid = territoryGetID(territory)
 
         output = ""
         output += @terrs.find(terrid).region
     end
 
-    def viewdetails(territory)
+    def territoryViewdetails(territory)
         terrtype = territory.class.to_s.downcase
         output = "<a href=\"#\" onclick=\""
         output += terrtype
         output += "sViewDetails('#{territory.id}');return false;\">View Details</a>"
     end
 
+    def territoryCheckin(territory)
+        if territory.class.to_s == "Terrout"
+            link_to 'Check In', [territory,checkin: territory.terrid], method: :delete, data: { confirm: "Are you sure you want to check in #{@terrs.find(territory.terrid).name}?" }
+        elsif territory.class.to_s == "Terr"
+            link_to 'Check In', {:controller => "terrins", :action => "new", :terrid => territory.id}
+        else
+            ""
+        end
+    end
+
+    def territoryCheckout(territory)
+        if territory.class.to_s == "Terrin"
+            link_to 'Check Out', [territory,checkout: territory.terrid], method: :delete, data: { confirm: "Are you sure you want to check out #{@terrs.find(territory.terrid).name}?" }
+        elsif territory.class.to_s == "Terr"
+            link_to 'Check Out', {:controller => "terrouts", :action => "new", :terrid => territory.id}
+        else
+            ""
+        end
+    end
+
     # Terrout Functions
-    def publisherTerrout(terrout)
+    def terroutPublisher(terrout)
         output = ""
         output += terrout.publisher
     end
 
-    def dateoutTerrout(terrout)
+    def terroutDateout(terrout)
         output = ""
         output += terrout.dateout.strftime("%m/%d/%y")
     end
     
-    def datedueTerrout(terrout)
+    def terroutDatedue(terrout)
         output = ""
         output += terrout.datedue.strftime("%m/%d/%y")
-    end
-
-    def checkinTerrout(terrout)
-        link_to 'Check In', [terrout,checkin: terrout.terrid], method: :delete, data: { confirm: "Are you sure you want to check in #{@terrs.find(terrout.terrid).name}?" }
-    end
-
-    # Terrin Functions
-    def checkoutTerrin(terrin)
-        link_to 'Check Out', [terrin,checkout: terrin.terrid], method: :delete, data: { confirm: "Are you sure you want to check out #{@terrs.find(terrin.terrid).name}?" }
-    end
-
-    # Terr Functions
-    def checkoutTerr(terr)
-        link_to 'Check Out', {:controller => "terrouts", :action => "new", :terrid => terr.id}
-    end
-
-    def checkinTerr(terr)
-        link_to 'Check In', {:controller => "terrins", :action => "new", :terrid => terr.id}
     end
 end
